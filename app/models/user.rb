519dc351fc187c8648190198a6036a9d05894f09
Jason Perkins <jasontperkins@gmail.com>
2015-04-22 15:12:46 -0400

    Adding friendly id to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Add friendly id and use the name as slug allowing the use of finders
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :finders]
 end
