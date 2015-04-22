6090d513cb87251d99bcc5bae38a6d02daf61713
Jason Perkins <jasontperkins@gmail.com>
2015-04-22 15:59:34 -0400

    Adding sidebar nav css and removing padding from sidebar and content, converting nav links into haml

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :finders]
+	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Add friendly id and use the name as slug allowing the use of finders
-  extend FriendlyId
-  friendly_id :name, use: [:slugged, :finders]
+
 end
